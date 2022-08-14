c28c5f8f47dcdfdabfc06f44ff306927944978e5
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-08-14 19:05:47 +0300

    add-file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const container = document.querySelector(".container");
 const image     = document.getElementById("music-image");
-const title     = document.querySelector(".music-details.title");
-const singer     = document.querySelector(".music-details.singer");
-const prev     = document.querySelector("#controls #prev");
-const play     = document.querySelector("#controls play");
-const next     = document.querySelector("#controls next");
+const audio     = document.querySelector("#audio");
+const title     = document.querySelector(".title");
+const singer    = document.querySelector(".singer");
+const prev      = document.querySelector("#prev");
+const play      = document.querySelector("#play");
+const next      = document.querySelector("#next");
 
 const player = new MusicPlayer(musicList);
+
 
 
 window.addEventListener("load",function(){
@@ -18,8 +20,54 @@ window.addEventListener("load",function(){
 
 function displayMusic(music){
 
-    this.title = music.getName();
-    this.singer = music.singer;
-    this.img = music.img;
-    this.file = music.file;
+    title.innerText  = music.getName();
+    image.src        = "img/" + music.img;
+    audio.src        = "mp3/" + music.file;
+}
+
+play.addEventListener("click",function(){
+
+    isMusicPlayer =  container.classList.contains("playing");
+    
+    isMusicPlayer ? pauseMusic() : playMusic();
+
+});
+
+prev.addEventListener("click",function(){
+
+    Musicprev();
+
+});
+
+next.addEventListener("click",function(){
+
+    Musicnext();
+
+});
+
+function Musicprev(){
+    player.previous();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+function Musicnext(){
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
+}
+
+
+function pauseMusic(){
+    container.classList.remove("playing");
+    play.classList="fa-solid fa-play";
+    audio.pause();
+}
+
+function playMusic(){
+    container.classList.add("playing");
+    play.classList="fa-solid fa-pause";
+    audio.play();
 }
